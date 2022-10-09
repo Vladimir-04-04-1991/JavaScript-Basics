4eda625bbbdcd568550cd9e04c065a3ca05e7e77
Vladimir <DEMON-DESPOD@yandex.ru>
2022-10-09 22:23:57 +0300

    add DZ1

---
+++ 1.base-concepts/task.js
@@ -0,0 +1,18 @@
+"use strict";
+function solveEquation(a, b, c) {
+  let arr = [];
+  let discriminant = b**2-4*a*c;
+  if(discriminant > 0) {
+    arr[0] = (-b + Math.sqrt(discriminant) )/(2*a);
+    arr[1] = (-b - Math.sqrt(discriminant) )/(2*a);
+  } else if(discriminant === 0) {
+    arr[0] = -b/(2*a);
+  }
+  return arr; // array
+}
+
+function calculateTotalMortgage(percent, contribution, amount, date) {
+  let totalAmount;
+
+  return totalAmount;
+}
